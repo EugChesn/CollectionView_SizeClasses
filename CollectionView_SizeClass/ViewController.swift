1d66067be671ede7330d4801c63ba1c49bee2036
Evgenii_Chistiakov <chesnok297@gmail.com>
2021-03-21 20:53:22 +0300

    Initial Commit

---
+++ CollectionView_SizeClass/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionView_SizeClass
+//
+//  Created by EvgeniiChistyakov on 21.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
